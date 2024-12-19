c0066abf0e3eef8588fc8a97be78ca1c35e55f7b
oktydeniz <oktydeniz07@gmail.com>
2024-12-19 15:54:05 +0300

    Initial Commit

---
+++ SpaceX-Launch/SpaceX_LaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_LaunchApp.swift
+//  SpaceX-Launch
+//
+//  Created by oktay on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_LaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
